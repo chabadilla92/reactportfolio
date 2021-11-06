dbaf085532a1e51a12de5ec775092ec2b4e65042
chabadilla92 <chabadilla92@gmail.com>
2021-11-05 17:41:44 -0700

    grid layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Projects from './components/Projects.js'
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Projects from "./components/Projects.js";
 
 function App() {
   return (
-    <div>
-      <Projects />
-
-
+    <div id="grid">
+      <nav>Navbar</nav>
+      <h1>Header</h1>
+      <main>Main</main>
+      
     </div>
   );
 }
