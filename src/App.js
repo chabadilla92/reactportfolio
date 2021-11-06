c4560cc6536308ffb69a679068540a1525e50213
chabadilla92 <chabadilla92@gmail.com>
2021-11-05 18:19:27 -0700

    nav styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import Projects from "./components/Projects.js";
 function App() {
   return (
     <div id="grid">
-      <nav>Navbar</nav>
+      <nav>
+        <ul id="sidenav">
+          <li><a href="#about"><i class="bi bi-person"></i></a></li>
+          <li><a href="#projects"><i class="bi bi-folder"></i></a></li>
+          <li><a href="#contact"><i class="bi bi-send"></i></a></li>
+        </ul>
+        </nav>
       <h1>Header</h1>
       <main>Main</main>
       
