e6ba02a9bc1a452661277c3075f9a5f8ac45dfcc
chabadilla92 <chabadilla92@gmail.com>
2021-11-19 14:30:58 -0800

    accordion done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,19 +3,21 @@ import Data from '../data/data.json'
 
 function Projects() {
     return (
-      <div class="container" id="projectscomponent">
+      <div class="container" id="accordionExample">
         {Data.map((project, index) => {
           return (
+
             <div class="text-center p-3" id={project.row}>
               <a class="btn btn-lg" data-toggle="collapse" href={project.link} id="button" role="button" aria-expanded="false" aria-controls="#collapseExample">{project.title}
               </a>
             </div>
+
           )
         })}
 
         {Data.map((project, index) => {
           return (
-            <div class="collapse p-4" id={project.id}>
+            <div class="collapse p-4" id={project.id} data-parent="#accordionExample">
                 <div class="card card-body" id="projectcards">{project.description}</div>
             </div>
           )
