93f0773e44ab2fb825ab4e3a86024c3393548013
chabadilla92 <chabadilla92@gmail.com>
2022-03-07 22:18:41 -0800

    ready for final steps

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,14 @@ function Footer() {
     return (
         <div id="footerdiv">
         <ul id="left">
-            <li>About</li>
-            <li>Blog</li>
+            <li class="about"><a href="#" data-replace="Get to know me!"><span><i class="bi bi-person-circle"></i></span></a></li>
+            <li class="blog"><a href="#" data-replace="Check out my blog!"><span><i class="bi bi-journal-code"></i></span></a></li>
         </ul>
         <ul id="right">
-            <li>Instagram</li>
-            <li>Github</li>
-            <li class="email"><a  data-replace="chabadilla92@gmail.com"><span>E-mail</span></a></li>
+            <li><a href="https://www.linkedin.com/in/christopher-abadilla/"><i class="bi bi-linkedin"></i></a></li>
+            <li><a href="https://github.com/chabadilla92"><i class="bi bi-github"></i></a></li>
+            <li class="email"><a data-replace="chabadilla92@gmail.com"><span><i class="bi bi-envelope-check"></i>
+</span></a></li>
         </ul>
         </div>
     )
