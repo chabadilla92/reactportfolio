754373dd1c0627492697be899ae3b82b8af7ca2e
chabadilla92 <chabadilla92@gmail.com>
2022-03-17 14:47:25 -0700

    finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
         <div>
         <ul id="footerdiv">
             <li class="about"><Link to="/about"><i class="bi bi-person-circle"></i></Link></li>
-            <li class="blog"><Link to="/blog"><i class="bi bi-journal-code"></i></Link></li>
+            {/* <li class="blog"><Link to="/blog"><i class="bi bi-journal-code"></i></Link></li> */}
             <li><a href="https://www.linkedin.com/in/christopher-abadilla/"><i class="bi bi-linkedin"></i></a></li>
             <li><a href="https://github.com/chabadilla92"><i class="bi bi-github"></i></a></li>
 
